18b9dd2b0e3916bb3a31c914df01f11f8629ac35
Muhammad  Arslan <arslanzafarr71@gmail.com>
2023-07-12 09:35:32 +0500

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import './App.css'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/Home/Home'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import PageNotFound from './components/PageNotFound/PageNotFound'
+import MovieDetails from './components/MovieDetails/MovieDetails'
 function App() {
 
   return (
     <>
-      <h1>hello</h1></>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/movie/:id' element={<MovieDetails />} />
+          <Route path='*' element={<PageNotFound />} />
+
+        </Routes>
+        <Footer />
+      </Router>
+
+    </>
   )
 }
 
